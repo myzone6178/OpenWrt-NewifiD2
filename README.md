8af65df7eec419199212f2813131c87660aea716
myzone6178 <71270736+myzone6178@users.noreply.github.com>
2022-11-11 19:42:35 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenWrt firmware for Newifi D2
+
+Auto build OpenWrt firmware for Newifi D2 via GitHub Actions
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/myzone6178/OpenWrt-Newifi_D2?style=for-the-badge&label=Download)](https://github.com/myzone6178/OpenWrt-Newifi_D2/releases/latest)
---
+++ diy-part1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
